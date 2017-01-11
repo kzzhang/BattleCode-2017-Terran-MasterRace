320100d3b47d088ef5044f098326466c36c4b662
PatzHum <patrick.hum@hotmail.com>
2017-01-10 23:32:35 -0500

    Init

---
+++ src/Alpha_v_0/Archon.java
@@ -0,0 +1,12 @@
+package Alpha_v_0;
+
+import battlecode.common.*;
+
+/**
+ * Created by Patrick on 2017-01-10.
+ */
+public class Archon extends Game{
+    public static void run() throws GameActionException{
+
+    }
+}
---
+++ src/Alpha_v_0/Game.java
@@ -0,0 +1,8 @@
+package Alpha_v_0;
+
+/**
+ * Created by Patrick  on 2017-01-10.
+ */
+public class Game {
+
+}
