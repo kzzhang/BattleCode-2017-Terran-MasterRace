bd49accf5b8e152086bb51dd32a8057101ed1c7c
kzzhang <kzhang2100@gmail.com>
2017-01-15 23:36:18 -0500

    Fixed gardener routine
    Runs around shaking trees once, need to fix gardener move direction
    decision

--- src/Alpha_v_0/Gardener.java
+++ src/Alpha_v_0/Gardener.java
@@ -20,15 +20,18 @@ public class Gardener {
             try {
 
                 // Listen for home archon's location
-                int xPos = rc.readBroadcast(0);
-                int yPos = rc.readBroadcast(1);
-                MapLocation archonLoc = new MapLocation(xPos,yPos);
+                //int xPos = rc.readBroadcast(0);
+                //int yPos = rc.readBroadcast(1);
+                //MapLocation archonLoc = new MapLocation(xPos,yPos);
 
                 // Generate a random direction
                 Direction dir = randomDirection();
 
                 boolean shaken = false;
+                Team self = rc.getTeam();
                 TreeInfo[] nearby = rc.senseNearbyTrees((float)7.0, NEUTRAL);
+                RobotInfo[] friendlyClose = rc.senseNearbyRobots((float)7, self);
+
                 for (int i = 0; i<nearby.length; i++){
                     if (rc.canShake(nearby[i].getID()) && nearby[i].getContainedBullets() >= 1){
                         rc.shake(nearby[i].getID());
@@ -37,18 +40,89 @@ public class Gardener {
                     }
                 }
 
+                //if no shake available
                 if (!shaken){
-                    MapLocation posClosestNeutral = nearby[0].getLocation();
-                    float distanceToTree = Distance.find(rc.getLocation(), posClosestNeutral);
+                    int numTrees = 0;
+                    MapLocation posClosestNeutral = null;
+                    float distanceToTree = -1;
+                    int start = 0;
 
-                    for (int i = 1; i<nearby.length; i++){
-                        if (Distance.find(rc.getLocation(), nearby[i].getLocation()) < distanceToTree){
+                    //find first tree nearby with bullets available
+                    for (int i = 0; i<nearby.length; i++){
+                        if (nearby[i].getContainedBullets() > 1.0){
                             posClosestNeutral = nearby[i].getLocation();
+                            distanceToTree = Distance.find(rc.getLocation(), posClosestNeutral);
+                            start = i;
+                            break;
                         }
                     }
 
-                    Direction direction = new Direction(rc.getLocation(), posClosestNeutral);
-                    rc.move(direction);
+                    //find closest tree nearby with bullets available and no friendly gardeners nearby
+                    boolean found = false;
+                    if (posClosestNeutral != null) {
+                        for (int i = start; i < nearby.length; i++) {
+                            System.out.println(Distance.find(rc.getLocation(), nearby[i].getLocation()));
+                            System.out.println(distanceToTree);
+                            System.out.println(nearby[i].getContainedBullets());
+                            if (Distance.find(rc.getLocation(), nearby[i].getLocation()) < distanceToTree && nearby[i].getContainedBullets() > 0) {
+                                System.out.println("found close!");
+                                for (RobotInfo data : friendlyClose) {
+                                    if (data.getType() == RobotType.GARDENER) {
+                                        System.out.println("Nearby!");
+                                        if (Distance.find(rc.getLocation(), nearby[i].getLocation()) > Distance.find(data.getLocation(), nearby[i].getLocation())) {
+                                            found = true;
+                                            System.out.println("Found!");
+                                            break;
+                                        }
+                                    }
+                                }
+                                System.out.println("Here!");
+                                if (!found) {
+                                    System.out.println("conflict!");
+                                    posClosestNeutral = nearby[i].getLocation();
+                                    distanceToTree = Distance.find(rc.getLocation(), posClosestNeutral);
+                                }
+                            }
+                        }
+                    }
+
+
+                    //take shortest path to closest tree
+
+                    Direction direction;
+                    if (posClosestNeutral != null) direction = new Direction(rc.getLocation(), posClosestNeutral);
+                    else{
+                        float archondir = 0;
+                        MapLocation[] archons = rc.getInitialArchonLocations(self);
+                        float distance = 99999;
+                        for (MapLocation archon : archons){
+                            if (Distance.find(rc.getLocation(), archon) < distance){
+                                distance = Distance.find(rc.getLocation(), archon);
+                                direction = new Direction(rc.getLocation(), archon);
+                                archondir = direction.radians;
+                            }
+                        }
+                        archondir += 3.14159;
+                        if (archondir > (3.14159*2)) archondir -= (float)(3.14159*2);
+                        direction = new Direction(archondir);
+                    }
+
+                    if (rc.canMove(direction)){
+                        rc.move(direction);
+                    }else {
+                        for (double i = 0.1; i < 3.14159; i+= 0.1){
+                            Direction positive = new Direction(direction.radians + (float)i);
+                            if (rc.canMove(positive)){
+                                rc.move(positive);
+                            }
+                            float negative = direction.radians - (float)i;
+                            if (negative < 0.0) { negative += (3.14159*2) ;}
+                            Direction negativeDir = new Direction(negative);
+                            if (rc.canMove(negativeDir)){
+                                rc.move(negativeDir);
+                            }
+                        }
+                    }
                 }
 
                 // Randomly attempt to build a soldier or lumberjack in this direction
@@ -59,9 +133,10 @@ public class Gardener {
                 }*/
 
                 // Move randomly
-                tryMove(randomDirection());
+                //tryMove(randomDirection());
 
                 // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
+                System.out.println("Bytes left: " + Integer.toString(Clock.getBytecodesLeft()));
                 Clock.yield();
 
             } catch (Exception e) {
